c61ad1d21bf3d9d084b8a126df074fedeef4a25a
Ashley Fightmaster <115577226+AshleyFightmaster@users.noreply.github.com>
2022-12-13 20:47:14 -0700

    add codewars problems

--- JavaScript-Homework.js
+++ JavaScript-Homework.js
@@ -24,7 +24,7 @@ and replaces it with the string "even index" */
 
 let Given_arr = ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
 
-function replaceEvens(arr){
+const replaceEvens = (arr) => {
     //code goes here
     for(let i in arr){
         if(parseInt(i) % 2 == 0){
@@ -42,31 +42,43 @@ console.log(replaceEvens(Given_arr))
 //Output arr == ["even index","Baseball","even index","Goku","even index","Rodger"]
 
 
-//codewars/
+// codewars ----- #1
+/* 
+Disemvowel Trolls 
+Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+ Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+ For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+*/
 let string = "This website is for losers LOL!"
 
 function disemvowel(str) {
-    arr=Array.from(str)
-    console.log(arr)
-    for (let i = 0; i < arr.length; i++){
-        if (arr[i] === 'a'){
-            arr.splice(i, 1)
-        } if (arr[i] === 'e'){
-            arr.splice(i, 1)
-        }if (arr[i] === 'i'){
-            arr.splice(i, 1)
-        }if (arr[i] === 'o'){
-            arr.splice(i, 1)
-        }if (arr[i] === 'u'){
-            arr.splice(i, 1)
-        }
-    }
-    console.log(arr)
-    new_string = arr.toString()
-    console.log(new_string)
-    
-    // list.toString()
-    // console.log(list.join(''))
-}
+    const a = /a/gi;
+    const e = /e/gi;
+    const i = /i/gi;
+    const o = /o/gi;
+    const u = /u/gi;
+    const newstr = str.replace(a,'').replace(e,'').replace(i,'').replace(o,'').replace(u,'');
+    return newstr;
+  }
 
   console.log(disemvowel(string))
+
+// codewars------- #2
+
+/*
+Multiply
+This code does not execute properly. Try to figure out why.
+*/
+
+function multiply(a, b){
+    let x = a * b
+    return x
+  }
+
+console.log(multiply(5,5))
